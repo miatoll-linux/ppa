cb1e5a74739d2c403d7cbe01b24cbfa6d8607ce2
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2023-01-25 21:47:24 +0200

    ci: patch python in debian/control when build the kernel

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-apt-get install linux-packaging-snippets
+apt-get install linux-packaging-snippets python-is-python3
 cd sources
 
 if [[ $_DEB_BUILD_TYPE == kernel ]]; 
 then
     rm debian/control
     debian/rules debian/control
+    # python-is-python3
+    sed -i 's/python,/python3,/' debian/control
 fi
 
 releng-build-package
